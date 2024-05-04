c80eb0afebd64707e8982a902569846e57a90720
Zhouchuan Huang <huangz@nju.edu.cn>
2024-05-04 10:19:55 +0800

    Update Tutorials.jl

--- src/Tutorials.jl
+++ src/Tutorials.jl
@@ -81,7 +81,9 @@ function build_tutorials()
     # Copy the Markdown first or the appended notebook links will add up.
     copy_markdown_files()
     append_notebook_links()
+    cd(PKGDIR)    
 end
 precompile(build_tutorials, ())
+
 
 end # module
